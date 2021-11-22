2df91376c573fa02e6418f995895b277c49e77c7
Vintaji <72357630+Vintaji@users.noreply.github.com>
2021-11-22 11:26:37 -0300

    Update loginScript.js

--- public/js/loginScript.js
+++ public/js/loginScript.js
@@ -27,7 +27,7 @@ const login = {
     },
 
     send: async () => {
-        const response = await fetch('http://localhost:3001/login', {
+        const response = await fetch('https://api.ricardosantana.xyz/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
